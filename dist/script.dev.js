b2023cc88019a77d9f3517f9325ce23f51bd59f8
AdeptusKustodius <56783828+AdeptusKustodius@users.noreply.github.com>
2020-11-07 00:30:43 +0400

    Add files via upload

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,8 +1,8 @@
 "use strict";
 
 var myBooks = document.body.querySelector('.myBooks');
-var myFavorbook = document.body.querySelector('.favorBooks');
-var readBook = document.body.querySelector('.readBook');
+var myFavorbooks = document.body.querySelector('.favorBooks');
+var readBooks = document.body.querySelector('.readBook');
 
 function uploadImage(event) {
   var reader = new FileReader();
@@ -18,7 +18,7 @@ function uploadImage(event) {
 }
 
 function ShowMyBook() {
-  var img = document.querySelector('.images');
+  var img = document.querySelector('.fDiv');
 
   if (myBooks.contains(img)) {
     myBooks.prepend(img);
@@ -26,12 +26,74 @@ function ShowMyBook() {
     for (var i = 0; i < window.localStorage.length; i++) {
       var res = window.localStorage.getItem(window.localStorage.key(i));
       var image = new Image();
+      var btnlks = document.createElement("button");
+
+      var _read = document.createElement("button");
+
+      var fdiv = document.createElement("div");
+      fdiv.innerHTML = "";
+      _read.innerHTML = "Read Book";
+      btnlks.innerHTML = "like!";
+      fdiv.setAttribute('class', 'fDiv thisBook');
+
+      _read.setAttribute('class', 'readBtn');
+
+      _read.setAttribute('id', 'read');
+
+      _read.setAttribute('onclick', 'read()');
+
+      btnlks.setAttribute('class', 'liksBtn');
+      btnlks.setAttribute('id', 'like');
+      btnlks.setAttribute('onclick', 'likes()');
       image.src = res;
       image.setAttribute('class', 'images');
-      myBooks.append(image);
+      myBooks.append(fdiv);
+      fdiv.append(image);
+      fdiv.append(btnlks);
+      fdiv.append(_read);
     }
   }
 
-  readBook.style.display = 'none';
-  myFavorbook.style.display = 'none';
+  myBooks.style.display = 'flex';
+  readBooks.style.display = 'none';
+  myFavorbooks.style.display = 'none';
+}
+
+function read() {
+  var parent = document.querySelector('.thisBook');
+  var elem1 = parent.querySelector('.images');
+  var clon = elem1.cloneNode(true);
+  var NewDiv = document.createElement("div");
+  NewDiv.innerHTML = "";
+  readBooks.append(NewDiv);
+  NewDiv.setAttribute('class', 'fDiv read');
+  NewDiv.append(clon);
+  console.log(2);
+}
+
+function likes() {
+  var parent = document.querySelector('.thisBook');
+  var elem1 = parent.querySelector('.images');
+  var elem2 = parent.querySelector('.readBtn');
+  var clon2 = elem2.cloneNode(true);
+  var clon = elem1.cloneNode(true);
+  var NewDiv = document.createElement("div");
+  NewDiv.innerHTML = "";
+  myFavorbooks.append(NewDiv);
+  NewDiv.setAttribute('class', 'fDiv');
+  NewDiv.append(clon);
+  NewDiv.append(clon2);
+  console.log(1);
+}
+
+function readBook() {
+  myBooks.style.display = 'none';
+  myFavorbooks.style.display = 'none';
+  readBooks.style.display = 'flex';
+}
+
+function ShowMyFavorBook() {
+  myBooks.style.display = 'none';
+  readBooks.style.display = 'none';
+  myFavorbooks.style.display = 'flex';
 }
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 let myBooks = document.body.querySelector('.myBooks');
-let myFavorbook = document.body.querySelector('.favorBooks');
-let readBook = document.body.querySelector('.readBook')
+let myFavorbooks = document.body.querySelector('.favorBooks');
+let readBooks = document.body.querySelector('.readBook')
 function uploadImage(event){
     let reader = new FileReader();
     let name = event.target.files[0].name;
@@ -16,21 +16,79 @@ function uploadImage(event){
     reader.readAsDataURL(event.target.files[0]);
 }
 function ShowMyBook(){
-    let img = document.querySelector('.images');
+    let img = document.querySelector('.fDiv');
     if(myBooks.contains(img)){
         myBooks.prepend(img);
     }else if (myBooks.contains(img)==false)
      {
 
     for (let i = 0; i < window.localStorage.length; i++) {
-        let res = window.localStorage.getItem(window.localStorage.key(i));	
+        let res = window.localStorage.getItem(window.localStorage.key(i));	 
         let image = new Image();
+        let btnlks = document.createElement("button");
+        let read = document.createElement("button")
+        let fdiv = document.createElement("div");
+        fdiv.innerHTML = "";
+        read.innerHTML = "Read Book"
+        btnlks.innerHTML = "like!";
+        fdiv.setAttribute('class','fDiv thisBook');
+        read.setAttribute('class','readBtn');
+        read.setAttribute('id','read');
+        read.setAttribute('onclick','read()');
+        btnlks.setAttribute('class','liksBtn');
+        btnlks.setAttribute('id','like');
+        btnlks.setAttribute('onclick','likes()');
         image.src = res;
         image.setAttribute('class','images');
-        myBooks.append(image);
-}
-}
-    readBook.style.display = 'none';
-    myFavorbook.style.display='none';
+        myBooks.append(fdiv)
+        fdiv.append(image);
+        fdiv.append(btnlks);
+        fdiv.append(read);
 }
 
+
+}
+    myBooks.style.display = 'flex';
+    readBooks.style.display = 'none';
+    myFavorbooks.style.display ='none';
+}
+function read(){
+    let parent = document.querySelector('.thisBook');
+    let elem1 =parent.querySelector('.images');
+    let clon = elem1.cloneNode(true);
+    let NewDiv = document.createElement("div");
+    NewDiv.innerHTML=""
+    readBooks.append(NewDiv);
+    NewDiv.setAttribute('class','fDiv read')
+    NewDiv.append(clon);
+    console.log(2)
+
+}
+
+function likes(){
+    let parent = document.querySelector('.thisBook');
+    let elem1 =parent.querySelector('.images');
+    let elem2= parent.querySelector('.readBtn');
+    let clon2 = elem2.cloneNode(true);
+    let clon = elem1.cloneNode(true);
+    
+    let NewDiv = document.createElement("div");
+    NewDiv.innerHTML=""
+    myFavorbooks.append(NewDiv);
+    NewDiv.setAttribute('class','fDiv')
+    NewDiv.append(clon);
+    NewDiv.append(clon2);
+    console.log(1)
+
+
+}
+function readBook(){
+    myBooks.style.display = 'none';
+    myFavorbooks.style.display='none';
+    readBooks.style.display = 'flex';
+}
+function ShowMyFavorBook(){
+    myBooks.style.display = 'none';
+    readBooks.style.display='none';
+    myFavorbooks.style.display = 'flex';
+}
