767784eadf9f122ef2ccfe2738aad8743f812dc2
AdeptusKustodius <56783828+AdeptusKustodius@users.noreply.github.com>
2020-11-10 11:03:37 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,52 +4,36 @@ let myFavorbooks = document.body.querySelector('.favorBooks');
 let readBooks = document.body.querySelector('.readBook')
 let download = document.body.querySelector('.download')
 
-function uploadText() {
-    let nameText = document.querySelector("input[type=text]").value;
-    let someText = document.querySelector("textarea[name=writing]").value;
-    localStorage.setItem("name",nameText);
-    localStorage.setItem("text",someText);
-    console.log('test')
-    console.log(nameText)
-    console.log(someText)
-}
-function uploadFile() {
-    let file = document.querySelector("input[type=file]").value;
-    window.localStorage.setItem(name,this.file);
-    console.log(file)
+let formLogin = document.forms.login;
+let formFile  = document.forms.file;
+let formWrite = document.forms.writing;
+function upload(){
+console.log(formLogin);
+console.log(formFile);
+console.log(formWrite);
+
 }
 
-    
-    function ShowMyBook(){    
-        for (let i = 0; i < window.localStorage.length; i++) {
-            let res = window.localStorage.getItem(window.localStorage.key(i));	 
-            let image = new  Image();
-            let btnlks = document.createElement("button");
-            let read = document.createElement("button")
-            let fdiv = document.createElement("div");
-            fdiv.innerHTML = "";
-            read.innerHTML = "Read Book"
-            btnlks.innerHTML = "like!";
-            fdiv.setAttribute('class','fDiv thisBook');
-            read.setAttribute('class','readBtn');
-            read.setAttribute('id','read');
-            read.setAttribute('onclick','read()');
-            btnlks.setAttribute('class','liksBtn');
-            btnlks.setAttribute('id','like');
-            btnlks.setAttribute('onclick','likes()');
-            image.src = res;
-            image.setAttribute('class','images');
-            myBooks.append(fdiv)
-            fdiv.append(image);
-            fdiv.append(btnlks);
-            fdiv.append(read);
-    }
-        myBooks.style.display = 'flex';
-        readBooks.style.display = 'none';
-        myFavorbooks.style.display ='none';
-        download.style.display ='none';
-    }
+async function upload(){
+let imgSet = await new Promise(resolve => formFile.toBlob(resolve, 'item/uploadFiles'));
 
+let formData = new FormData()
+formData.set('login',formLogin);
+formData.set('text',imgSet,'text.txt');
+
+let response = await fetch('/item/uploadFiles',{
+    method:'POST',
+    body:formData
+})
+let result = await response.json();
+console.log(result.message);
+}
+function ShowMyBook(){
+    myBooks.style.display = 'flex';
+    readBooks.style.display = 'none';
+    myFavorbooks.style.display ='none';
+    download.style.display ='none';
+}
 function read(){
     let keys1 = localStorage.getItem('name');
     let keys2 = localStorage.getItem('text');
